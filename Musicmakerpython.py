5ae1ea1910f485c13a92bc95c7abf18ca45081d2
mahimayam <mahimay2010@gmail.com>
2025-04-12 13:59:04 -0400

    Add files via upload

---
+++ Musicmakerpython.py
@@ -0,0 +1,43 @@
+print("hello world")
+from pysine import sine
+c = 260
+csharp = 275
+d = 292
+dsharp = 309
+e = 328
+f = 347
+fsharp = 368
+g = 390
+gsharp = 413
+a = 437
+asharp = 463
+b = 491
+c2 = 520
+d2 = 584
+def Birthday():
+    sine(frequency=390, duration=0.25)
+    sine(frequency=390, duration=0.25)
+    sine(frequency=437, duration=0.5)
+    sine(frequency=390, duration=0.5)
+    sine(frequency=520, duration=0.5)
+    sine(frequency=491, duration=0.5)
+    sine(frequency=390, duration=0.25)
+    sine(frequency=390, duration=0.25)
+    sine(frequency=437, duration=0.5)
+    sine(frequency=390, duration=0.5)
+    sine(frequency=584, duration=0.5)
+    sine(frequency=520, duration=0.5)
+    sine(frequency=390, duration=0.25)
+    sine(frequency=390, duration=0.25)
+    sine(frequency=780, duration=0.5)
+    sine(frequency=656, duration=0.5)
+    sine(frequency=520, duration=0.5)
+    sine(frequency=491, duration=0.5)
+    sine(frequency=437, duration=0.5)
+    sine(frequency=684, duration=0.25)
+    sine(frequency=684, duration=0.25)
+    sine(frequency=656, duration=0.5)
+    sine(frequency=c2, duration=0.5)
+    sine(frequency=d2, duration=0.5)
+Birthday()
+
